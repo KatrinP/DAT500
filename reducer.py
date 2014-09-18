bfc40187a584d4a106f5dba821fa9a7c97f5d4c7
tulinkry <k.tulinger@seznam.cz>
2014-09-18 22:29:23 +0200

    map and reduce

---
+++ reducer.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+from operator import itemgetter
+import sys
+
+current = None
+current_count = 0
+ngram = None
+
+for line in sys.stdin: # read input from STDIN
+  line = line.strip() # remove leading and trailing whitespace
+  try:
+    ngram, count = line.split('\t', 1) # parse the input we got from email_count_mapper.py, two words, tab delimited
+  except:
+    print "error: ", line
+    continue
+  try: # convert count from str to int, if not possible discard this input
+    count = int(count)
+  except ValueError:
+    continue
+
+  if current == ngram: #this works because mapper output is sorted by key (ngram) by Hadoop before feeding to reducer input
+    current_count += count #if email domain hasn't changed continue counting its occurances
+  else:
+    if current: #if email domain has changed output count for previous email domain to STDOUT and start counting for new email domain
+      print '%s\t%s' % (current, current_count)
+    current_count = count
+    current = ngram
+
+# output the last email domain
+if current == ngram:
+  print '%s\t%s' % (current, current_count)
